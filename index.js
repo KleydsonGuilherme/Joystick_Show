ad25edd0ffbdd5753cf983b489f5116e2cd78af7
KleydsonGuilherme <62815097+KleydsonGuilherme@users.noreply.github.com>
2021-08-18 22:21:12 -0300

    ARQUIVOS DO PROJETO
    upload do projetos joystick_show - treino frontEnd

---
+++ index.js
@@ -0,0 +1,29 @@
+function home(){
+    window.alert('Home');
+}
+
+function news(){
+    window.alert('News');
+}
+
+function otherModels(){
+    window.alert('Other Models');
+}
+
+function about(){
+    window.alert("I'm Kleydson Guilherme, begginer in FrontEnd");
+}
+
+function workWithUs(){
+    window.alert('Contact: Kleydsong@gmail.com');
+}
+
+function github(){
+    window.alert('Github.com/kleydsonguilherme');
+}
+function linkedin(){
+    window.alert('Linkedin.com/in/kleydsong');
+}
+function mail(){
+    window.alert('Kleydsong@gmail.com');
+}
