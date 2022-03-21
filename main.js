9208cd8340eed15feb4e2e5421b3ca45037c1cab
Kleydson Guilherme <Kleydsong@gmail.com>
2022-03-21 20:06:23 -0300

    3d effect

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+const card = document.querySelector(".Joy");
+const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
+const THRESHOLD = 10;
+
+
+function handleHover(e){
+    const { clientX, clientY, currentTarget } = e;
+    const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+    const horizontal = (clientX - offsetLeft) / clientWidth;
+    const vertical = (clientY - offsetTop) / clientHeight;
+    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(3);
+    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(3);
+
+    card.style.transform = `perspective(${clientWidth}px) 
+    rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+}
+
+function resetStyles(e){
+    card.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+}
+
+if(!motionMatchMedia.matches){
+    card.addEventListener("mousemove",handleHover);
+    card.addEventListener("mouseleave", resetStyles);
+}
+
+
 function home(){
     window.alert('Home');
 }
